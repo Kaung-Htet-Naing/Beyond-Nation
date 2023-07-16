3a2e6d1d124bdfd208e2e20b3f2d24b9e17811d6
Kaung Htet Naing <kaunghtetnaing@Kaungs-MacBook-Pro.local>
2023-07-16 23:38:58 +0700

    add protected routes

--- middleware.ts
+++ middleware.ts
@@ -6,31 +6,28 @@ export async function middleware(request: NextRequest, response: NextResponse) {
 	const path = request.nextUrl.pathname;
 
 	//Return to /login if don't have a session
-	if (!session) {
-		return NextResponse.redirect(
-			new URL("http://localhost:3000/signup", request.url)
-		);
+	if (!session && path !== "/login" && path !== "/signup") {
+		return NextResponse.redirect(new URL("/signup", request.url));
 	}
 
-	//Call the authentication endpoint
 	const responseAPI = await fetch("http://localhost:3000/api/login", {
 		headers: {
 			Cookie: `session=${session?.value}`,
 		},
 	});
 
-	//Return to /login if token is not authorized
-	if (responseAPI.status !== 200) {
+	if (responseAPI.status !== 200 && path !== "/login" && path !== "/signup") {
 		return NextResponse.redirect(new URL("/signup", request.url));
 	}
-	if (responseAPI.status == 200) {
+	if (
+		responseAPI.status == 200 &&
+		(path === "/signup" || path === "/signup" || path === "/")
+	) {
 		return NextResponse.redirect(new URL("/e-commerce", request.url));
 	}
 	return NextResponse.next();
 }
 
-//Add your protected routes
 export const config = {
-	// matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-	matcher: ["/something"],
+	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", "/"],
 };
